9bcdae22f24d7a39131cccd8e078997aedb3e3a4
Tyler Burkhardt <tylerburkhardt4@gmail.com>
2025-05-27 14:17:46 -0700

    sign up page

---
+++ web/src/app/actions/auth.ts
@@ -0,0 +1,6 @@
+"use server"
+
+export async function signup(formData: FormData) {
+  // validate
+  // hit api
+}
---
+++ web/src/app/auth/sign-up/page.tsx
@@ -0,0 +1,23 @@
+import { signup } from "@/app/actions/auth";
+import styles from './page.module.css';
+
+export default function SignUpPage() {
+  
+  return (
+    <form className={styles.root} action={signup}>
+      <div>
+        <label htmlFor="name">Name</label>
+        <input type="text" name="name" id="name" />
+      </div>
+      <div>
+        <label htmlFor="email">Email</label>
+        <input type="email" name="email" id="email" />
+      </div>
+      <div>
+        <label htmlFor="password">Password</label>
+        <input type="password" name="password" id="password" />
+      </div>
+      <button type="submit">Create Account</button>
+    </form>
+  );
+}
